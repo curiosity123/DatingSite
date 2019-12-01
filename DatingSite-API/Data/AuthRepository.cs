67295913becd3814514dea5f2ecf1d7e7548e74b
Łukasz Adach <lukasz.adach7@gmail.com>
2019-12-01 23:05:02 +0100

    changes in db  reinit

--- DatingSite-API/Data/AuthRepository.cs
+++ DatingSite-API/Data/AuthRepository.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text;
 using System.Threading.Tasks;
 using DatingSite_API.Models;
 
@@ -11,7 +13,7 @@ namespace DatingSite_API.Data
         {
             _context = context;
         }
-
+        #region Public method
         public Task<User> Login(string username, string password)
         {
             throw new System.NotImplementedException();
@@ -19,14 +21,34 @@ namespace DatingSite_API.Data
 
         public async Task<User> Register(User user, string password)
         {
-             throw new System.NotImplementedException();
-            //await return null;
-          // await _context.User.
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHashSalt(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt; 
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
         }
+
+
 
         public Task<bool> UserExists(string username)
         {
             throw new System.NotImplementedException();
         }
+
+        #endregion
+
+        #region Private methods
+        private void CreatePasswordHashSalt(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
     }
+    #endregion
 }
