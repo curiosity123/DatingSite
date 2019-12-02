125bf0c3dc87b9627bf96440325e670b9519f93d
Łukasz Adach <lukasz.adach7@gmail.com>
2019-12-02 22:08:08 +0100

    now login generate JWT token

--- DatingSite-API/Data/AuthRepository.cs
+++ DatingSite-API/Data/AuthRepository.cs
@@ -56,15 +56,14 @@ namespace DatingSite_API.Data
         #region Private methods
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
-
+                hmac.Key = passwordSalt;
                 var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
                 for (int i = 0; i < computedHash.Length; i++)
                 {
                     if (computedHash[i] != passwordHash[i])
                         return false;
-
                 }
             }
             return true;
