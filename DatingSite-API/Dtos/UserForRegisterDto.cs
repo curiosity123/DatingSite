50bd8581d1e53d6895ab1177daca06212dc177d3
Łukasz Adach <lukasz.adach7@gmail.com>
2019-12-02 20:58:06 +0100

    added validation

--- DatingSite-API/Dtos/UserForRegisterDto.cs
+++ DatingSite-API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingSite_API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required(ErrorMessage="Nazwa użytkownika jest wymagana")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage="Hasło jest wymagane")]
+        [StringLength(12, MinimumLength=6,ErrorMessage="Hasło musi składać się z minimum 6 i max 12 znaków")]
         public string Password { get; set; }
 
     }
