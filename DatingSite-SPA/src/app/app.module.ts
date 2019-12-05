07ca674726ce78979b4844cb098e11d740f81551
Łukasz Adach <lukasz.adach7@gmail.com>
2019-12-06 00:23:06 +0100

    register component

--- DatingSite-SPA/src/app/app.module.ts
+++ DatingSite-SPA/src/app/app.module.ts
@@ -8,20 +8,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
-      NavComponent   
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule
    ],
-   providers: [AuthService],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
