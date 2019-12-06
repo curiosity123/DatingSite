774e610902c76266a2cc3e3f5942e6545d47948b
Łukasz Adach <lukasz.adach7@gmail.com>
2019-12-06 23:38:01 +0100

    register method works

--- DatingSite-SPA/src/app/register/register.component.ts
+++ DatingSite-SPA/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from 'util';
 
 @Component({
   selector: 'app-register',
@@ -6,17 +8,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('success');}, 
+      error => {console.log(error);});
   }
 
   cancel() {
