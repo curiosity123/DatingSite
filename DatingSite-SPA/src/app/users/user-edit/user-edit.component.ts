ca4765638ce9bfc5ba2b80a3ebb4427792700261
Łukasz Adach <lukasz.adach7@gmail.com>
2019-12-12 23:08:37 +0100

    added save button and info  in edit form

--- DatingSite-SPA/src/app/users/user-edit/user-edit.component.ts
+++ DatingSite-SPA/src/app/users/user-edit/user-edit.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/_services/user.service';
 import { User } from 'src/app/models/User';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user-edit',
@@ -11,13 +13,19 @@ import { User } from 'src/app/models/User';
 export class UserEditComponent implements OnInit {
 
   user: User;
+  @ViewChild('editForm', {static: false}) editForm: NgForm;
 
 
-  
-  constructor( private route: ActivatedRoute) { }
+  constructor( private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
 this.route.data.subscribe(data => this.user = data.user);
+  }
+
+
+  updateUser() {
+    this.alertify.success('updated!!!!');
+    this.editForm.reset(this.user);
   }
 
 }
